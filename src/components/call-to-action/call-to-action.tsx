5b3cec97bed1fe584f2972bb10402ce472e9fef8
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-09-24 18:43:25 -0300

    Add background image

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -5,8 +5,9 @@ import Link from "next/link";
 
 export default function CallToAction() {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 hidden md:block bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
           {" "}
           <div className="p-4 bg-cyan-300 w-fit rounded-full">
